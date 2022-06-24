8d1ee3c0ff0dfc2cd60705159ad47501165abfdb
lev <levananenkov@yandex.ru>
2022-06-24 21:37:11 +0500

    Ex7

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import requests
 
-res = requests.get("https://playground.learnqa.ru/api/long_redirect")
+requests_type = ["post", "get", "put", "delete"]
+params_method = ["POST", "GET", "PUT", "DELETE", "LOL"]
 
-print(len(res.history))
-print(res.url)
+for i in requests_type:
+    print(requests.request(method=i, url="https://playground.learnqa.ru/ajax/api/compare_query_type").text)
+
+for req_type in requests_type:
+    print(f"\n Тип метода {req_type}")
+    for method in params_method:
+        data = None
+        params = None
+        if req_type == "get":
+            params = {"method": method}
+            print(f"Значение params = {params}")
+        else:
+            data = {"method": method}
+            print(f"Значение data = {data}")
+
+        print(requests.request(method=req_type,
+                               url="https://playground.learnqa.ru/ajax/api/compare_query_type",
+                               params=params,
+                               data=data).text)
