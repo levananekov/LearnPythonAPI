c186b298c781b2b1baf7a9032f599e8d3bbb8747
lev <levananenkov@yandex.ru>
2022-06-24 20:20:19 +0500

    Ex5

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-import requests
+import json
 
-print(requests.get("https://playground.learnqa.ru/api/get_text").text)
+json_text = '{"messages":[' \
+                '{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},' \
+                '{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}' \
+            ']}'
+
+json_messages = json.loads(json_text).get("messages")
+second_json_message = json_messages[1].get("message")
+
+print(second_json_message)
