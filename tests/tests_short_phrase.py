9b5a0ec8b5f3a48e5e604c78b080e9f40b96e946
lev <levananenkov@yandex.ru>
2022-07-01 23:11:17 +0500

    Ex12

--- tests/tests_short_phrase.py
+++ tests/tests_short_phrase.py
@@ -1,5 +1,5 @@
 import requests
 
-def test_homework_cookie():
+def test_homework_headers():
     response = requests.get(url="https://playground.learnqa.ru/api/homework_cookie")
-    assert response.cookies.get('HomeWork'), "Cookie does't matter Homework"
+    assert response.headers.get('Set-Cookie'), "Cookie does't matter Homework"
