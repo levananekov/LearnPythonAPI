6cf106b69f090864324c1696bc35c109afe5e415
lev <levananenkov@yandex.ru>
2022-07-03 20:21:49 +0500

    Ex13

--- tests/tests_short_phrase.py
+++ tests/tests_short_phrase.py
@@ -1,5 +1,33 @@
+import pytest
 import requests
 
-def test_homework_headers():
-    response = requests.get(url="https://playground.learnqa.ru/api/homework_cookie")
-    assert response.headers.get('Set-Cookie'), "Cookie does't matter Homework"
+parametrizes= [
+    (
+        'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
+        {'platform': 'Mobile', 'browser': 'No', 'device': 'Android'}
+    ),
+    (
+        'Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1',
+        {'platform': 'Mobile', 'browser': 'Chrome', 'device': 'iOS'}
+    ),
+    (
+        'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+        {'platform': 'Googlebot', 'browser': 'Unknown', 'device': 'Unknown'}
+    ),
+    (
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0',
+        {'platform': 'Web', 'browser': 'Chrome', 'device': 'No'}
+    ),
+    (
+        'Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+        {'platform': 'Mobile', 'browser': 'No', 'device': 'iPhone'}
+    )
+]
+
+
+@pytest.mark.parametrize("user_agent, expected_values", parametrizes)
+def test_homework_headers(user_agent, expected_values):
+    response = requests.get(url=" https://playground.learnqa.ru/ajax/api/user_agent_check",
+                            headers={"User-Agent": user_agent})
+    for i in expected_values:
+        assert response.json().get(i) == expected_values.get(i), f"Неверное значение у значения'{i}' в ответе"
