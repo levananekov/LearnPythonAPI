97a0f361a0820adf42588ec23f110f68d5c8ccea
lev <levananenkov@yandex.ru>
2022-07-04 22:51:31 +0500

    lol kek

--- tests/tests_short_phrase.py
+++ tests/tests_short_phrase.py
@@ -1,5 +1,5 @@
 import requests
 
 def test_homework_headers():
-    response = requests.get(url="https://playground.learnqa.ru/api/homework_cookie")
-    assert response.headers.get('Set-Cookie'), "Cookie does't matter Homework"
+    response = requests.get(url="https://playground.learnqa.ru/api/homework_header")
+    assert response.headers.get("x-secret-homework-header"), "Cookie does't matter Homework"
